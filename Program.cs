969a2e5a8203e9f3560f4ef7d4d5447b9fd34369
PickCool <A.Svyatyuk.FIT.121.20@knute.edu.ua>
2021-09-14 19:52:15 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Practichna1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Github");
+            Console.ReadKey();
         }
     }
 }
