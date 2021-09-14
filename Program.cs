043bf1e23b46f15a3c08feb346291f16bdd8ded3
PickCool <A.Svyatyuk.FIT.121.20@knute.edu.ua>
2021-09-14 19:49:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practichna1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
